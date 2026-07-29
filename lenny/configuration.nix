710e7a4a0fa21eeed2ac611cbcd358d518bd7633
bepro@odoo <bepro@odoo.com>
2026-07-29 23:12:23 +0200

    More WIP

--- lenny/configuration.nix
+++ lenny/configuration.nix
@@ -55,23 +55,37 @@
     ];
   };
 
-  # Display Manager (Login Screen)
-  services.displayManager.ly.enable = true;
+  # Ensure greetd is not enabled anywhere by default (hosts can override if needed)
+  services.greetd.enable = false;
+
+  # Animations  "doom", "colormix", "matrix"
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "matrix";
+      bigclock = true;
+      # --- Color Settings (0xAARRGGBB) ---
+      # Background color of dialog box (Black)
+      bg = "0x00000000";
+      # Foreground text color (Cyan: #00FFFF)
+      fg = "0x0000FFFF";
+      # Border color (Red: #FF0000)
+      border_fg = "0x00FF0000";
+      # Error message color (Red)
+      error_fg = "0x00FF0000";
+      # Clock color (Purple: #800080)
+      clock_color = "#800080";
+    };
+  };
 
   # Compositor (Niri)
   programs.niri.enable = true;
 
   # Basic system packages
   environment.systemPackages = [
-    pkgs.nixfmt-rfc-style
+    pkgs.nixfmt
     pkgs.vscodium
   ];
-
-  # DankMaterialShell (provides locker, topbar, launcher)
-  programs.dms-shell = {
-    enable = true;
-    systemd.enable = true;
-  };
 
   home-manager.users.bert-proesmans =
     {
@@ -95,10 +109,6 @@
                 tap
                 natural-scroll
             }
-        }
-        // Force 1280x720 on the QEMU virtual display
-        output "Virtual-1" {
-            mode "1280x720"
         }
         layout {
             gaps 10
