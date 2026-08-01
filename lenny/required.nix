522189bfd816a7bf0cfdf77066a66512710eaedf
Bert Proesmans <bproesmans@hotmail.com>
2026-08-01 22:27:15 +0000

    Additional hardware services

--- lenny/required.nix
+++ lenny/required.nix
@@ -20,9 +20,54 @@
     # Enables automatic low-power mode
     wifi.powersave = true;
   };
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-old --delete-older-than 7d";
+  };
   nix.settings.trusted-public-keys = [
     "development.local-1:Wq31nOqkJWq1EIMabjKnLSCdlPwb5xmsZDur+RZNE4I="
   ];
+
+  # TODO; Doesn't work without singular host config entry point!
+  # system.copySystemConfiguration = true;
+  system.stateVersion = "26.05";
+  time.timeZone = "Europe/Brussels";
+
+  services.logind.settings.Login = {
+    HandleLidSwitch = "suspend";
+    HandleLidSwitchExternalPower = "lock";
+    HandleLidSwitchDocked = "ignore";
+  };
+
+  systemd.sleep.settings.Sleep = {
+    AllowSuspendThenHibernate = "yes";
+    HibernateDelaySec = "30m";
+  };
+
+  # Prevent (Intel) CPU overheating
+  services.thermald.enable = true;
+  services.tlp = {
+    # Automatically adjust system power profiles
+    enable = false;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20; # ??
+
+      # Optional helps save long term battery health
+      START_CHARGE_THRESH_BAT0 = 75; # charge below 75
+      STOP_CHARGE_THRESH_BAT0 = 80; # stop charging at or above 80
+    };
+  };
+
 
   security.sudo-rs = {
     enable = true;
@@ -31,11 +76,6 @@
   };
 
   services.openssh.enable = true;
-
-  # TODO; Doesn't work without singular host config entry point!
-  # system.copySystemConfiguration = true;
-  system.stateVersion = "26.05";
-  time.timeZone = "Europe/Brussels";
 
   # DEBUG
   users.users.bert-proesmans.password = "testing123"; # DEBUG
