0b34ee09fcfa9fa96878c3ed439e110cdd368ca4
Bert Proesmans <bproesmans@hotmail.com>
2026-08-01 22:27:35 +0000

    Expand internationalisation config

--- nixos/internationalisation.nix
+++ nixos/internationalisation.nix
@@ -5,4 +5,24 @@
   console.useXkbConfig = true;
 
   time.timeZone = lib.mkDefault "Etc/UTC";
+
+  i18n = {
+      defaultLocale = lib.mkDefault "en_GB.UTF-8";
+      extraLocales = [
+        "en_GB.UTF-8/UTF-8"
+        "nl_BE.UTF-8/UTF-8"
+      ];
+      # REF; https://man.archlinux.org/man/locale.7
+      extraLocaleSettings = {
+        LC_NUMERIC = lib.mkDefault "nl_BE.UTF-8";
+        LC_TIME = lib.mkDefault "nl_BE.UTF-8";
+        LC_MONETARY = lib.mkDefault "nl_BE.UTF-8";
+        LC_PAPER = lib.mkDefault "nl_BE.UTF-8";
+        LC_NAME = lib.mkDefault "nl_BE.UTF-8";
+        LC_ADDRESS = lib.mkDefault "nl_BE.UTF-8";
+        LC_TELEPHONE = lib.mkDefault "nl_BE.UTF-8";
+        LC_MEASUREMENT = lib.mkDefault "nl_BE.UTF-8";
+        LC_IDENTIFICATION = lib.mkDefault "nl_BE.UTF-8";
+      };
+    };
 }
